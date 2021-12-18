d53e280972e2e2e8423d8e5f91b418e34a387974
dorukuzucu <dorukuzucu@gmail.com>
2021-12-18 20:09:31 +0300

    datasets refactoring

--- datasets/inpainting.py
+++ datasets/inpainting.py
@@ -9,26 +9,24 @@ from torch.utils.data import Dataset
 import datasets.utils as dataset_utils
 
 
-class BsdDataset(Dataset):
+class BaseDataset(Dataset):
     items: List[str]
 
-    def __init__(self, dataset_path: str, img_format="jpg",
+    def __init__(self, img_format="jpg",
                  size: Union[int, Tuple[int, int]] = None,
                  grayscale: bool = False):
-        self.dataset_path = dataset_path
         self.image_format = img_format
         self.size = size
         self.gray_scale = grayscale
 
         self.transforms = None
-        self.initialize()
 
     def initialize(self):
         self._list_dataset()
         self._build_transforms()
 
     def _list_dataset(self):
-        self.items = dataset_utils.list_directory(dir_path=self.dataset_path, content_type=self.image_format)
+        raise NotImplementedError()
 
     def _build_transforms(self):
         transform_list = []
@@ -48,3 +46,17 @@ class BsdDataset(Dataset):
         img = dataset_utils.read_img(img_path)
         img = self.transforms(img)
         return img
+
+
+class BsdDataset(BaseDataset):
+    items: List[str]
+
+    def __init__(self, dataset_path: str, img_format="jpg", size: Union[int, Tuple[int, int]] = None,
+                 grayscale: bool = False):
+        super().__init__(img_format, size, grayscale)
+
+        self.dataset_path = dataset_path
+        self.initialize()
+
+    def _list_dataset(self):
+        self.items = dataset_utils.list_directory(dir_path=self.dataset_path, content_type=self.image_format)
