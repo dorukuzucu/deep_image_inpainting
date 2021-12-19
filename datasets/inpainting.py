4eba2583dd70b7f17c000bcb81187f3c1416358c
dorukuzucu <dorukuzucu@gmail.com>
2021-12-19 15:06:30 +0300

    dataset class return type is updated

--- datasets/inpainting.py
+++ datasets/inpainting.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset
 import datasets.utils as dataset_utils
 
 
-class BaseDataset(Dataset):
+class _BaseDataset(Dataset):
     items: List[str]
 
     def __init__(self, img_format="jpg",
@@ -41,14 +41,22 @@ class BaseDataset(Dataset):
     def __len__(self):
         return len(self.items)
 
+    def _mask_input(self, img):
+        channels, height, width = img.size()
+        mask_bbox = dataset_utils.generate_random_bbox((width, height))
+        mask = dataset_utils.bbox_to_mask(mask_bbox, width, height)
+        img_clone = img.clone()
+        return img_clone * mask
+
     def __getitem__(self, index):
         img_path = self.items[index]
         img = dataset_utils.read_img(img_path)
         img = self.transforms(img)
-        return img
+        masked_img = self._mask_input(img)
+        return masked_img, img
 
 
-class BsdDataset(BaseDataset):
+class BsdDataset(_BaseDataset):
     items: List[str]
 
     def __init__(self, dataset_path: str, img_format="jpg", size: Union[int, Tuple[int, int]] = None,
